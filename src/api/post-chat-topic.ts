7db86b740c525bd6c43e8ed57cbe95b341fb1a37
Saeed-sk <skandaryan.m2b@gmail.com>
2025-06-29 03:55:21 +0330

    change create chat with topic

--- src/api/post-chat-topic.ts
+++ src/api/post-chat-topic.ts
@@ -3,9 +3,18 @@ import axios from "@/lib/axios";
 
 export async function postChatTopic(topic_id: number): Promise<ChatMessage[]> {
     try {
-        const response = await axios.post <ApiResponse<ChatMessage[]>>(`/api/new/chat/topic`, {
-            topic_id: topic_id,
-        });
+        const response = await axios.post<ApiResponse<ChatMessage[]>>(
+            `/api/new/chat/topic`,
+            {
+                topic_id: topic_id
+            },
+            {
+                headers: {
+                    'Pragma': 'no-cache',
+                    'Expires': '0'
+                }
+            }
+        );
         return response.data.data;
     } catch (error: any) {
         console.log(error)
